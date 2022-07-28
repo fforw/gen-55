f03e351aa59dba58ecbce82b78c332ba7bd6e373
Sven Helmberger <fforw@gmx.de>
2022-07-28 15:15:51 +0200

    Anti-wallpaper

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import weightedRandom from "./weightedRandom"
 import randomPalette, { randomPaletteWithBlack } from "./randomPalette"
 import Color from "./Color"
 import AABB from "./AABB"
-
+import { voronoi } from "d3-voronoi"
+import { polygonCentroid, polygonArea } from "d3-polygon"
 
 const PHI = (1 + Math.sqrt(5)) / 2;
 const TAU = Math.PI * 2;
@@ -38,51 +39,135 @@ const greenA = Color.from("#081")
 const greenB = Color.from("#b9aa00")
 
 
-function drawCrown(x,y,color, ratio = 0.9, ratio2 = 0)
+const overdraw = 1.15
+
+function randomPoints(v)
 {
-    const colorA = Color.from(color).mix(shadow, ratio2).toRGBHex();
-    const colorB = Color.from(color).mix(shadow, ratio).toRGBHex();
+    const {width, height} = config
 
-    ctx.save()
-    ctx.beginPath()
-
-    const aabb = new AABB()
-
-    for (let i=0; i < 800; i++)
+    let pts = []
+    const count = 1000 + 100 * Math.random()
+    for (let i = 0; i < count; i++)
     {
-        const angle = TAU * Math.random()
-        const r = 140 * Math.sqrt(Math.random())
-
-        const size = Math.round(2 + Math.pow(Math.random(),4) * 8)
-        let x2 = x + Math.cos(angle) * r
-        let y2 = y + Math.sin(angle) * r
-
-        aabb.add(x2,y2)
-        
-        ctx.moveTo(
-            x2 + size,
-            y2,
-        )
-        ctx.arc(x2,y2,size,0,TAU, true)
+        pts.push([
+            Math.random() * width * overdraw,
+            Math.random() * height * overdraw,
+        ])
     }
-    ctx.clip()
-
-    let gradient = ctx.createLinearGradient(
-        aabb.minX, aabb.minY,
-        aabb.maxX, aabb.maxY
-    )
-    gradient.addColorStop(0,colorA)
-    gradient.addColorStop(1,colorB)
-    ctx.fillStyle = gradient
-
-    ctx.fillRect(
-        aabb.minX, aabb.minY,
-        aabb.width, aabb.height
-    )
-
-    ctx.restore()
+    return pts
 }
 
+function relax(v,pts, relaxCount = 3)
+{
+    for (let i=0; i < relaxCount; i++)
+    {
+        const diagram = v(pts);
+        const polygons = diagram.polygons();
+        pts = polygons.map(poly => poly && polygonCentroid(poly));
+    }
+    return pts
+}
+
+const limit = 1000
+
+
+function randomPair(palette)
+{
+    const indexA = 0 | Math.random() * palette.length
+    let indexB
+    do
+    {
+        indexB = 0 | Math.random() * palette.length
+    } while(indexA === indexB)
+
+    return [
+        palette[indexA],
+        palette[indexB]
+    ]
+}
+
+
+function shorten(centroid, x0, y0, amount)
+{
+    const dx = x0 - centroid[0]
+    const dy = y0 - centroid[1]
+    const l = Math.sqrt(dx*dx+dy*dy);
+
+    if (l <= amount)
+    {
+        return [x0,y0]
+    }
+    
+    return [
+        centroid[0] + dx * (l - amount) / l,
+        centroid[1] + dy * (l - amount) / l
+    ]
+}
+
+
+function shrink(polygon, centroid, amount = 10)
+{
+    const last = polygon.length - 1
+    const vertexA = Math.floor(Math.random() * polygon.length)
+    const vertexB = vertexA === last ? 0 : vertexA + 1
+
+    const [x0,y0] = polygon[vertexA]
+    const [x1,y1] = polygon[vertexB]
+
+    polygon[vertexA] = shorten(centroid, x0, y0, amount)
+    polygon[vertexB] = shorten(centroid, x1, y1, amount)
+}
+
+
+function drawPolygon(polygon, palette)
+{
+    const [colorA, colorB] = randomPair(palette)
+    let even = false
+    let area
+    const centroid = polygonCentroid(polygon)
+
+    do
+    {
+        const last = polygon.length - 1
+        const [x1, y1] = polygon[last]
+
+        ctx.fillStyle = palette[0|Math.random() * palette.length] 
+        ctx.beginPath()
+        ctx.moveTo(
+            x1 | 0,
+            y1 | 0
+        )
+
+        for (let j = 0; j < polygon.length; j++)
+        {
+            const [x1, y1] = polygon[j]
+
+            ctx.lineTo(x1 | 0, y1 | 0)
+        }
+        ctx.fill()
+
+        shrink(polygon, centroid, 20)
+        even = !even
+
+        area = polygonArea(polygon)
+    }while(area > limit)
+}
+
+
+function drawPolygons(diagram, palette)
+{
+    const { width, height } = config
+
+    ctx.save()
+    ctx.translate(Math.round(-(width * overdraw - width ) * 0.5), Math.round(-(height * overdraw - height ) * 0.5))
+    const polygons = diagram.polygons()
+    for (let i = 0; i < polygons.length; i++)
+    {
+        const polygon = polygons[i]
+        drawPolygon(polygon.slice(), palette)
+    }
+    ctx.restore()
+}
 
 domready(
     () => {
@@ -106,26 +191,15 @@ domready(
 
             const palette = randomPaletteWithBlack()
 
-            ctx.fillStyle = "#fff"
+            ctx.fillStyle = Math.random() < 0.5 ? "#000" : "#fff"
             ctx.fillRect(0,0,width,height)
 
-            for (let i=0; i < 5; i++)
-            {
-                const angle = TAU * Math.random()
-                const r = 150 * Math.sqrt(Math.random())
+            const v = voronoi().extent([[0, 0], [width * overdraw, height * overdraw]])
+            const pts = relax(v, randomPoints(v), 10);
 
-                let x2 = cx + Math.cos(angle) * r
-                let y2 = cy + Math.sin(angle) * r
-
-                drawCrown(
-                    x2, y2,
-                    greenA.mix(greenB, Math.random()).toRGBHex(),
-                    0.9,
-                    i < 2 ? 0.9 : 0.6
-                )
-            }
-
-
+            const diagram = v(pts)
+            ctx.strokeStyle = "#044";
+            drawPolygons(diagram, palette)
         }
 
         paint()
